02ce03a66e410a67db0555a1a5fa27018821b5e8
SiamKing <thesiameseelectric@gmail.com>
2020-01-17 12:49:19 -0700

    Created App.js and updated index.js to render app.js

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <p>Hello</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
