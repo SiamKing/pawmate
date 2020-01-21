1a8c01c21b3706b93b40a65e4a34446458d4d425
SiamKing <thesiameseelectric@gmail.com>
2020-01-21 15:39:23 -0700

    Updated render to include AnimalFinder component

--- app/javascript/components/containers/AnimalsContainer.js
+++ app/javascript/components/containers/AnimalsContainer.js
@@ -1,13 +1,19 @@
 import React from "react"
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import AnimalFinder from '../animals/AnimalFinder'
 
 
 class AnimalsContainer extends React.Component {
   render () {
     return (
-      <Container>
-        <Row className="justify-content-md-center">
+      <Container fluid="true" className="">
+        <Row className="flex-fill flex-column flex-sm-row">
+          <Col md={2} className="sidebar bg-dark vh-100 text-light pt-5">
+            <AnimalFinder />
+          </Col>
           Animals
         </Row>
       </Container>
