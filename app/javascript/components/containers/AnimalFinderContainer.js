9fed6e2eb09254c405e14fec3d833932ceb17954
SiamKing <thesiameseelectric@gmail.com>
2020-01-23 14:34:46 -0700

    Created AnimalFinderContainer for business logic

---
+++ app/javascript/components/containers/AnimalFinderContainer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import AnimalFinder from '../animals/AnimalFinder'
+
+class AnimalFinderContainer extends React.Component {
+  render () {
+    return (
+      <AnimalFinder />
+    );
+  }
+}
+
+export default AnimalFinderContainer
