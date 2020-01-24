789818653c268551ab1df13a6d4138b5ddc0fd43
SiamKing <thesiameseelectric@gmail.com>
2020-01-23 17:07:59 -0700

    Created root reducer for redux router

---
+++ app/javascript/reducers/RootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+
+import manageAnimals from './manageAnimals'
+
+const createRootReducer = (history) => combineReducers({
+  router: connectRouter(history),
+  animals: manageAnimals
+})
+export default createRootReducer
